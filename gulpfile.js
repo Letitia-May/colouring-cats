f4d6ec4909ceea8769339858412636a96bd3edf2
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-11-24 18:40:28 +0100

    Update gulp version, Remove the now ignored directory node_modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+gulp.task('styles', () => {
+  return gulp
+    .src('sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/'));
 });
+
+gulp.task('default', gulp.series(['styles']));
