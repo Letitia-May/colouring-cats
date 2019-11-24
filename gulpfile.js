0adc09c29f929024a89f25b8a069f57a9a5a1963
Chloe Kuypers <chloeletitiamay@gmail.com>
2019-11-24 21:32:58 +0100

    Fix gulpfile now that directory structure has changed

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ gulp.task('styles', () => {
   return gulp
     .src('src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css/'));
+    .pipe(gulp.dest('./src/css/'));
 });
 
 gulp.task('default', gulp.series(['styles']));
